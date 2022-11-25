a48ddf3956644e67ff669d7680c4b472fbb54941
tebibux <tebibusolomon79@gmail.com>
2022-11-25 07:54:16 +0300

    js started

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import DomMaker from "./main/domMaker";
+
+const container = document.querySelector('.container')
---
+++ src/main/domMaker.js
@@ -0,0 +1,19 @@
+class DomMaker{
+	constructor(type, className, Id, InHTML, InText){
+		this.type = type;
+		this.className = className;
+		this.Id = Id;
+		this.InHTML = InHTML;
+		this.InText = InText;
+	}
+	maker(){
+		const domElement = document.createElement(`${this.type}`);
+		domElement.className = `${this.className}`;
+		domElement.id = `${this.Id}`;
+		domElement.innerHTML = `${this.InHTML}`;
+		domElement.innerText = `${this.InText}`;
+		return domElement
+	}
+}
+
+export default DomMaker;
