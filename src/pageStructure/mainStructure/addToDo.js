e3bf66c9357b58dd79770da0def163b2b3273d70
tebibux <tebibusolomon79@gmail.com>
2022-11-26 20:30:59 +0300

    popup dome half finished

--- src/pageStructure/mainStructure/addToDo.js
+++ src/pageStructure/mainStructure/addToDo.js
@@ -1,12 +1,32 @@
 import DomMaker from "../../main/domMaker";
 // creates a todo dashboard data entry form
-function addToDo(){
+function addToDo() {
 	const addToDo_toArrayD = new DomMaker('div', 'dashBoard', '', '', '');
 	const addToDo_toArray = addToDo_toArrayD.maker();
-	
+	// adding title to the addtodo page form
 	const formTitleD = new DomMaker('input', 'formTitle', 'formTitle', '', '');
 	const formTitle = formTitleD.maker();
+	formTitle.classList.add('hightMaker');
+	formTitle.setAttribute('placeholder', 'Input Title')
+	formTitle.setAttribute("required", "");
 	addToDo_toArray.appendChild(formTitle);
+
+	// adding description to the addtodo page form
+	const fromDescD = new DomMaker('input', 'formTitle', 'formDesc', '', '');
+	const fromDesc = fromDescD.maker();
+	fromDesc.classList.add('hightMaker');
+	fromDesc.setAttribute("required", "");
+	addToDo_toArray.appendChild(fromDesc);
+
+	// adding description to the addtodo page form
+	const formDateD = new DomMaker('input', 'formTitle', 'formDate', '', '');
+	const formDate = formDateD.maker();
+	formDate.classList.add('hightMaker');
+	formDate.setAttribute('type', 'date')
+	formDate.setAttribute("required", "");
+	addToDo_toArray.appendChild(formDate);
+
+
 
 
 	return addToDo_toArray
