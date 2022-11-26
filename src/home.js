23af826ceb7b2fb2cc8ce0644391ebd02bf14be3
tebibux <tebibusolomon79@gmail.com>
2022-11-26 20:41:43 +0300

    home import redundency managed

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-import './styles/style.css';
+// import './styles/style.css';
 import headerDom from "./pageStructure/pageHeader"; // imports the header DOM
 import footerDom from './pageStructure/pageFooter'; // imports the footer DOM
 import mainDom from './pageStructure/pageMain'; // imports the main DOM
